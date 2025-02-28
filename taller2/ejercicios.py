31990f84a95672addbf8809de311db0ea86bdb47
emilioaa18 <eatehortua20@hotmail.com>
2025-02-27 20:59:58 -0500

    Add files via upload

---
+++ taller2/ejercicios.py
@@ -0,0 +1,21 @@
+#Ej1
+
+lista = []
+
+for i in range (10):
+    numeros = int(input("Ingrese un numero: "))
+    lista.append(numeros)
+
+def procesar_lista (numeros):
+    suma = sum(numeros)
+    print(filter)
+    resultado = filter(lambda n:n>5, numeros)
+
+
+
+#ej2
+numeros = [2,4,13,9,15]
+resultado = map(lambda n: n ** 2, numeros)
+resultado= filter(lambda n:n >50, resultado)
+print (list(resultado))
+
